feec15865b76f12569e8679066843e1fa71dc091
sanjarjelet13 <126490244+sanjarjelet13@users.noreply.github.com>
2024-03-18 16:01:54 -0400

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/general/navbar";
 import Footer from "@/components/general/footer";
 import PageLayout from "@/components/pagelayout";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
           <PageLayout>{children}</PageLayout>
           <Footer />
         </Providers>
+        <Analytics />
       </body>
     </html>
   );
